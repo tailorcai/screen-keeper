6e48abbded3cd731f9519e0b92ee4e05ec3e26d5
蔡峰 <caifeng@guolele.com>
2020-03-20 10:38:12 +0800

    Revert "to server"
    This reverts commit 00a3d102b2657fc9b7582f06953b54f967143d07.

--- capture.js
+++ capture.js
@@ -1,35 +1,30 @@
 var exec = require("child_process").execSync;
 var fs = require("fs");
-var crypto = require("crypto");
+// var crypto = require("crypto");
 var os = require("os");
 var moment = require("moment");
-// var path = require("path");
-var request = require("request");
-
+var path = require("path");
 var config = {
-	capture_console_only: false,
-	upload_url: "http://ss.irisleciel.com:8010/upload",
-	// upload_url: "http://127.0.0.1:8010/upload",
-	hostname: os.hostname()
+    capture_console_only: false,
 };
 
-// var root_path = function() {
-//     if (process.platform == "darwin")
-//         return "/Volumes/Screens/" + os.hostname()
-//     if (process.platform == "win32") 
-//         return "X:/user/screens/"+os.hostname()
-// }()
+var root_path = function() {
+    if (process.platform == "darwin")
+        return "/Volumes/Screens/" + os.hostname()
+    if (process.platform == "win32") 
+        return "X:/user/screens/"+os.hostname()
+}()
 
-// function mkdirsSync(dirname) {
-//     if (fs.existsSync(dirname)) {
-//       return true;
-//     } else {
-//       if (mkdirsSync(path.dirname(dirname))) {
-//         fs.mkdirSync(dirname);
-//         return true;
-//       }
-//     }
-//   }
+function mkdirsSync(dirname) {
+    if (fs.existsSync(dirname)) {
+      return true;
+    } else {
+      if (mkdirsSync(path.dirname(dirname))) {
+        fs.mkdirSync(dirname);
+        return true;
+      }
+    }
+  }
 
 function capture() {
 	if (process.platform == "darwin" && config.capture_console_only && exec("stat -f '%Su' /dev/console").toString().trim() != os.userInfo().username) throw "Not console";
@@ -40,12 +35,13 @@ function capture() {
 			if (cp.stdout.toString().split('\n').find(x => x.startsWith('>')).substr(46, 4) == "Disc") throw "Inactive session"; // will capture black screen otherwise
 		}
 	}
-	
 	var filedate = new Date();
-	var p1 = moment(filedate).format("YYYY-MM-DD_HH_mm-ss")
-	var filename = `${config.hostname}_${p1}-${encodeURIComponent(crypto.randomBytes(16).toString('base64'))}.jpg`
-	var filepath = os.tmpdir() + '/' + filename;
-
+	var p1 = moment(filedate).format("mm-ss")
+	var filename = `${p1}.jpg`
+	var filepath = root_path + moment(filedate).format("/YYYY-MM-DD/HH/") + filename;
+    // console.log( filepath )
+    mkdirsSync( path.dirname(filepath)  )
+    // filepath = filepath + '/' + filename
 	if (process.platform == "darwin") {
 		exec(`screencapture -x -C -t jpg "${filepath}"`);
 	} else if (process.platform == "win32") {
@@ -54,28 +50,7 @@ function capture() {
 		exec(`import -silent -window root "${filepath}"`);
 	}
 
-	// console.log( `yes! ${filepath}`)
-	var formData = {
-		image: {
-		  value:  fs.createReadStream(filepath),
-		  options: {
-			filename: filename,
-			contentType: 'image/jpg'
-		  }
-		}
-	  };
-	request.post({
-        url:config.upload_url, 
-        formData: formData
-		}, 
-		function optionalCallback(err, httpResponse, body) {
-			fs.unlinkSync(filepath);
-			if (err) {
-				return console.error('upload failed:', err);
-			}
-			console.log('Upload successful!  Server responded with:', body);
-	});
-
+    console.log( `yes! ${filepath}`)
 }
 
 module.exports = capture;
